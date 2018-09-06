04978f49fc3c9a5f83b9e3f1029093d7b1f91a69
Ivan <beatkeen@gmail.com>
2018-09-06 16:43:23 +0300

    Improve UI with "blessed"

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+from blessed import Terminal
 from scrapy.crawler import CrawlerProcess
 from word_spider import WordSpider
 
 
 if __name__ == '__main__':
-    filename_out = 'out.csv'
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-    process.crawl(WordSpider, filename_out=filename_out)
-    process.start() # the script will block here until the crawling is finished
+    term = Terminal() 
+    with term.fullscreen():
+        filename_out = 'out.csv'
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'LOG_LEVEL': 'ERROR',
+        })
+        process.crawl(WordSpider, filename_out=filename_out, term=term)
+        process.start() # the script will block here until the crawling is finished
--- word_spider.py
+++ word_spider.py
@@ -4,25 +4,22 @@ from word_item import WordItem
 
 class WordSpider(scrapy.Spider):
     name = 'word'
-    base_url = 'https://www.mdbg.net/chinese/dictionary?wdqb='
 
-    def __init__(self, filename_out):
+    def __init__(self, filename_out, term):
+        self.base_url = 'https://www.mdbg.net/chinese/dictionary?wdqb='
+        self.ui = self.SpiderUi(term)
         self.filename_out = filename_out
-        self.start_urls = [self.ask_pinyin()]
-
-    def ask_pinyin(self):
-        search_query = input('Enter pinyin of the character (q to exit): ')
-        if search_query.lower() != 'q':
-            return self.base_url + search_query
+        self.start_urls = [self.base_url + self.ui.get_search_query()]
 
     def parse(self, response):
         item = self.parse_mdbg(response)
         if item:
             self.write_item(item)
 
-        next_search = self.ask_pinyin()
-        if next_search:
-            yield response.follow(next_search, self.parse)
+        question = '\nOne more query ([y]/n)? '
+        if self.ui.yes_or_no(question):
+            next_search = self.ui.get_search_query()
+            yield response.follow(self.base_url+next_search, self.parse)
 
     def parse_mdbg(self, response):
         '''Parse mdbg.net search results.'''
@@ -38,23 +35,35 @@ class WordSpider(scrapy.Spider):
             item['hanzi'] = row.css(hanzi_css).xpath(normalize).extract_first()
             item['pinyin'] = row.css(pinyin_css).xpath(normalize).extract_first()
             item['english'] = row.css(english_css).xpath(normalize).extract_first()
-            if self.ask_user(item) == True:
+            if self.ui.check_item(item) == True:
                 return item
-
-    def ask_user(self, item):
-        '''Ask user if parsed data is good.
-        Return True if user enters 'y', False if 'n'. 
-        '''
-        text = '{} ({}) - {}\nIs this what you\'re looking for (y/n)?\n'
-        text = text.format(item['hanzi'], item['pinyin'], item['english'])
-        answer = input(text).lower()
-        while answer not in ['y', 'n']:
-            answer = input('Wrong input.\n').lower()
-        return answer == 'y'
 
     def write_item(self, item):
         '''Append an item to a csv file.'''
-        with open(self.filename, 'a') as fout:
+        with open(self.filename_out, 'a') as fout:
             line = '\t'.join([item['hanzi'], item['pinyin'], item['english']])
             fout.write(line) 
             fout.write('\n')
+
+    class SpiderUi:
+        def __init__(self, term):
+            self.term = term
+
+        def get_search_query(self):
+            print(self.term.clear, end='', flush=True)
+            search_query = input('Enter pinyin: ')
+            return search_query
+
+        def check_item(self, item):
+            '''Ask user if parsed data is good.
+            Return True if user enters 'y', False if 'n'. 
+            '''
+            question = '{} ({}) - {}\nIs this what you\'re looking for ([y]/n)? '
+            question = question.format(item['hanzi'], item['pinyin'], item['english'])
+            return self.yes_or_no(question)
+
+        def yes_or_no(self, question):
+            answer = input(question).lower()
+            while answer not in ['', 'y', 'n']:
+                answer = input('Wrong input.\n').lower()
+            return answer in ['', 'y']
