d2485b812628803351af3025447209b1b2bb4b5b
Ivan <beatkeen@gmail.com>
2018-09-10 15:19:10 +0300

    Move output function to the word model

--- word_model.py
+++ word_model.py
@@ -9,4 +9,16 @@ class Word:
         self.notes = ''
 
     def __str__(self):
-        return '{} ({}) - {}'.format(self.hanzi, self.pinyin, self.english)
+        format_str = '{} ({}) - {}\n'
+        return format_str.format(self.hanzi, self.pinyin, self.english)
+
+    def write_to_file(self, filename_out):
+        '''Append the word to a tsv file.'''
+        fields = (self.hanzi,
+                  self.pinyin,
+                  self.english,
+                  str(self.strokes))
+        with open(filename_out, 'a') as fout:
+            line = '\t'.join(fields)
+            fout.write(line) 
+            fout.write('\n')
